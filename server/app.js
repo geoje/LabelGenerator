bac5876bf65cec280c279103e0b2bf8f3f74cdcc
Gyeongho Yang <chchch1213@naver.com>
2022-09-25 00:38:32 -0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/api/greeting", (req, res) => res.send("Hello World!"));
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => res.sendFile("client/build"));
+
+app.listen(PORT);
