a2bf062ad40e50c37bc070d06b5d9c41bb08001c
Gyeongho Yang <chchch1213@naver.com>
2022-09-25 15:00:51 -0700

    Start fullpage design

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Pageable from "pageable";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="pg-wrapper pg-vertical">
+      <div id="container" className="pg-container">
+        <div
+          data-anchor="page-1"
+          id="page-1"
+          className="pg-page pg-active"
+        ></div>
+        <div data-anchor="page-2" id="page-2" className="pg-page"></div>
+        <div data-anchor="page-3" id="page-3" className="pg-page"></div>
+        <div data-anchor="page-4" id="page-4" className="pg-page"></div>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+new Pageable("#container", {
+  childSelector: "[data-anchor]", // CSS3 selector string for the pages
+  anchors: [], // define the page anchors
+  pips: true, // display the pips
+  animation: 300, // the duration in ms of the scroll animation
+  delay: 0, // the delay in ms before the scroll animation starts
+  throttle: 50, // the interval in ms that the resize callback is fired
+  orientation: "vertical", // or horizontal
+  swipeThreshold: 50, // swipe / mouse drag distance (px) before firing the page change event
+  freeScroll: false, // allow manual scrolling when dragging instead of automatically moving to next page
+  navPrevEl: false, // define an element to use to scroll to the previous page (CSS3 selector string or Element reference)
+  navNextEl: false, // define an element to use to scroll to the next page (CSS3 selector string or Element reference)
+  infinite: false, // enable infinite scrolling (from 0.4.0)
+  slideshow: {
+    // enable slideshow that cycles through your pages automatically (from 0.4.0)
+    interval: 3000, // time in ms between page change,
+    delay: 0, // delay in ms after the interval has ended and before changing page
+  },
+  events: {
+    wheel: true, // enable / disable mousewheel scrolling
+    mouse: true, // enable / disable mouse drag scrolling
+    touch: true, // enable / disable touch / swipe scrolling
+    keydown: true, // enable / disable keyboard navigation
+  },
+  easing: function (currentTime, startPos, endPos, interval) {
+    // the easing function used for the scroll animation
+    return -endPos * (currentTime /= interval) * (currentTime - 2) + startPos;
+  },
+  onInit: function () {
+    // do something when the instance is ready
+  },
+  onUpdate: function () {
+    // do something when the instance updates
+  },
+  onBeforeStart: function () {
+    // do something before scrolling begins
+  },
+  onStart: function () {
+    // do something when scrolling begins
+  },
+  onScroll: function () {
+    // do something during scroll
+  },
+  onFinish: function () {
+    // do something when scrolling ends
+  },
+});
