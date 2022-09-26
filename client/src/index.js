a4f92056359525cf3930149cd65811ea58987843
Gyeongho Yang <chchch1213@naver.com>
2022-09-26 00:37:59 -0700

    Delete unused template and Merge App.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import WebFont from "webfontloader";
+import Pageable from "pageable";
+
+import Draw from "./page/draw";
+import Print from "./page/print";
 
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+function App() {
+  // Download Font
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Poppins"],
+      },
+    });
+
+    // Set Pageable
+    var anchors = [].slice.call(
+      document.querySelector(".anchors").firstElementChild.children
+    );
+    new Pageable("#container", {
+      freeScroll: true,
+      onInit: (data) =>
+        anchors.forEach((anchor, i) =>
+          anchor.firstElementChild.classList.toggle("active", i === data.index)
+        ),
+      onFinish: (data) =>
+        anchors.forEach((anchor, i) =>
+          anchor.firstElementChild.classList.toggle("active", i === data.index)
+        ),
+    });
+  });
+
+  return (
+    <>
+      <div className="anchors">
+        <ul>
+          <li>
+            <a href="#page-1">Draw</a>
+          </li>
+          <li>
+            <a href="#page-2">Print</a>
+          </li>
+        </ul>
+      </div>
+      <div className="pg-wrapper pg-vertical">
+        <div id="container" className="pg-container">
+          <Draw />
+          <Print />
+        </div>
+      </div>
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
---
+++ client/src/page/print.js
@@ -0,0 +1,11 @@
+import "./print.css";
+
+function Print() {
+  return (
+    <div data-anchor="page-2" id="page-2" className="pg-page">
+      <h1>Print</h1>
+    </div>
+  );
+}
+
+export default Print;
