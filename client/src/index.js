0f97420dace5179e3aedbcf08f1ceac54089c2e1
Gyeongho Yang <chchch1213@naver.com>
2022-10-02 13:49:18 -0700

    Remove pageable and Add Mantine

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
+import { MantineProvider } from "@mantine/core";
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import WebFont from "webfontloader";
-import Pageable from "pageable";
 
+import Nav from "./page/nav";
 import Draw from "./page/draw";
 import Print from "./page/print";
 
@@ -16,48 +17,19 @@ function App() {
         families: ["Poppins"],
       },
     });
-
-    // Set Pageable
-    var anchors = [].slice.call(
-      document.querySelector(".anchors").firstElementChild.children
-    );
-    new Pageable("#container", {
-      freeScroll: true,
-      onInit: (data) =>
-        anchors.forEach((anchor, i) =>
-          anchor.firstElementChild.classList.toggle("active", i === data.index)
-        ),
-      onFinish: (data) =>
-        anchors.forEach((anchor, i) =>
-          anchor.firstElementChild.classList.toggle("active", i === data.index)
-        ),
-    });
   });
 
   return (
     <>
-      <div className="anchors">
-        <ul>
-          <li>
-            <a href="#page-1">Draw</a>
-          </li>
-          <li>
-            <a href="#page-2">Print</a>
-          </li>
-        </ul>
-      </div>
-      <div className="pg-wrapper pg-vertical">
-        <div id="container" className="pg-container">
-          <Draw />
-          <Print />
-        </div>
-      </div>
+      <Nav />
+      <Draw />
+      <Print />
     </>
   );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <MantineProvider withGlobalStyles withNormalizeCSS>
     <App />
-  </React.StrictMode>
+  </MantineProvider>
 );
