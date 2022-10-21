c151eb9812d6f2173e48f19e7df0a3c147f53a9e
Gyeongho Yang <chchch1213@naver.com>
2022-10-21 08:25:21 -0700

    Change directory

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/api/greeting", (req, res) => res.send("Hello World!"));
 
-app.use(express.static("client/build"));
-app.get("*", (req, res) => res.sendFile("client/build"));
+app.use(express.static("../client/build"));
+app.get("*", (req, res) => res.sendFile("../client/build"));
 
 app.listen(5000);
