884917d0f2316425b4a911a2bf6307d3c09e537d
Gyeongho Yang <chchch1213@naver.com>
2022-10-20 23:07:31 -0700

    Set port to 8080

--- server/app.js
+++ server/app.js
@@ -6,4 +6,4 @@ app.get("/api/greeting", (req, res) => res.send("Hello World!"));
 app.use(express.static("client/build"));
 app.get("*", (req, res) => res.sendFile("client/build"));
 
-app.listen(5000);
+app.listen(8080);
