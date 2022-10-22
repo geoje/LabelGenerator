3dc2d639f01095c92657302e86d0c2e714f57b3b
Gyeongho Yang <chchch1213@naver.com>
2022-10-22 12:48:31 -0700

    All reset package, upgrade and repair

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 5000;
 
 app.get("/api/greeting", (req, res) => res.send("Hello World!"));
 
-app.use(express.static("../client/build"));
-app.get("*", (req, res) => res.sendFile("../client/build"));
+const staticPath = path.resolve(__dirname, "../client/build");
+app.use(express.static(staticPath));
+app.get("*", (req, res) => res.sendFile(staticPath));
 
-app.listen(port);
+app.listen(port, () =>
+  console.log("\x1b[32m%s\x1b[0m", `Server running at http://127.0.0.1:${port}`)
+);
