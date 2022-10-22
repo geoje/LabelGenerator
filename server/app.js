1eb68239a2c53ba1817306b2c94a2fedbc3bcad2
Gyeongho Yang <chchch1213@naver.com>
2022-10-21 19:20:08 -0700

    Unused nodemon and set env port

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/api/greeting", (req, res) => res.send("Hello World!"));
 
 app.use(express.static("../client/build"));
 app.get("*", (req, res) => res.sendFile("../client/build"));
 
-app.listen(8080);
+app.listen(port);
