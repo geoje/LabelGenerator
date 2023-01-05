b561058e84dbded381e758d7bb52f9a0069329af
3DBioCAD_Tech <chchch1213@naver.com>
2023-01-05 11:21:55 -0800

    Add dotenv and port

--- server/app.js
+++ server/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 4860;
+
+process.env.NODE_ENV =
+  process.env.NODE_ENV &&
+  process.env.NODE_ENV.trim().toLowerCase() == "development"
+    ? "development"
+    : "production";
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
+dotenv.config({
+  path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}.local`),
+});
+const port = process.env.PORT || 5000;
 
 const staticPath = path.resolve(__dirname, "../client/build");
 app.use(express.static(staticPath));
