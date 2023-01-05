b68f263c8b7575e0b8743d828339f55ee357828b
3DBioCAD_Tech <chchch1213@naver.com>
2023-01-05 11:24:15 -0800

    Remove little server-side code

--- server/app.js
+++ server/app.js
@@ -15,8 +15,6 @@ const port = process.env.PORT || 5000;
 
 const staticPath = path.resolve(__dirname, "../client/build");
 app.use(express.static(staticPath));
-app.get("*", (req, res) => res.sendFile(staticPath));
+app.get("*", (_req, res) => res.sendFile(staticPath));
 
-app.listen(port, () => {
-  // console.log("\x1b[32m%s\x1b[0m", `Server running at http://127.0.0.1:${port}`)
-});
+app.listen(port, () => {});
