228f32a48811329700ab3d594acedffee2c84c03
Gyeongho Yang <chchch1213@naver.com>
2023-03-02 23:17:13 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -109,7 +109,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>{t("Label Generator")}</title>
+        <title>{t("Data") + t("Label Generator")}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
